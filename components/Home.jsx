1bf8f9dab640f98ca2836bc21c46357f2b16f806
Gabriel M <gmoreno9109@gmail.com>
2023-11-27 11:49:06 +0100

    second commit

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,30 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import styles from "@/styles/Home.module.css"
-import Movie from './Movie'
-import 'antd/dist/antd.css';
-import { Popover, Button } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import styles from "@/styles/Home.module.css";
+import Movie from "./Movie";
+import "antd/dist/antd.css";
+import { Popover, Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
-  const [likedMovies, updateList] = useState([])
-  const [movieList, updateMovies] = useState([])
+  const [likedMovies, updateList] = useState([]);
+  const [movieList, updateMovies] = useState([]);
   const updateLikedMovies = (movieTitle) => {
-    if(likedMovies.find(movie => movie === movieTitle)){
-      updateList(likedMovies.filter(el=> el !== movieTitle))
-    } else updateList(([...likedMovies, movieTitle]))
-    console.log(likedMovies);
+    if (likedMovies.find((movie) => movie === movieTitle)) {
+      updateList(likedMovies.filter((el) => el !== movieTitle));
+    } else updateList([...likedMovies, movieTitle]);
   };
-  
-  // const infoMovie = [
-  //   {poster: "forrestgump.jpg", title: "Forrest Gump", overview:"A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.",averageRating:9.2, countRating:22_705},
-  //   {poster: "thedarkknight.jpg", title: "The Dark Knight", overview:"Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.",averageRating:8.5, countRating:27_547},
-  //   {poster: "yourname.jpg", title: "Your name", overview:"High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.",averageRating:8.5, countRating:8_691},
-  //   {poster: "ironman.jpg", title: "Iron Man", overview:"After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",averageRating:7.6, countRating:22_7726},
-  //   {poster: "inception.jpg", title: "Inception", overview:"Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.",averageRating:8.4, countRating:31_546},
-  // ]
-  
-  
+
   let popoverMovies = likedMovies.map((movie, i) => {
     return (
       <li key={i}>
@@ -38,58 +28,58 @@ function Home() {
     );
   });
 
+  const funk = async function () {
+    const req = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=99a959537352a345ae214a14d1a4c48c"
+    );
+    const res = await req.json();
+    const data = updateMovies(Object.entries(res.results));
+  };
 
-const funk = async function(){
-  const req = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=99a959537352a345ae214a14d1a4c48c");
-  const res = await req.json()
-  const data = updateMovies(Object.entries(res.results))
-  // return data
-}
+  useEffect(() => {
+    funk();
+  }, []);
 
-useEffect(() => {
-  funk()
-}, []);
-  
   const movie = movieList.map((info, i) => {
-    console.log(info[1]);
-    // let overview = info[1].overview
-    // if(overview.kength > 250) overview.slice(0, 250) + "..."
-  const isLiked = likedMovies.find(mov => mov === info[1].title);
-  return (<Movie key={i} className={styles.movieCard} {...info[1]} /*overview={overview}*/ poster= {`https://image.tmdb.org/t/p/w500${info[1].poster_path}`} /*poster={info[1].poster_path} title={info[1].original_title} overview={info[1].overview} averageRating={info[1].vote_average} countRating={info[1].vote_count}*/ updateLikedMovies={updateLikedMovies} isLiked={isLiked} />)
- })
-
-// data.map((info, i) => {
-//   console.log(info[1]);
-//   const isLiked = likedMovies.find(mov => mov === info[1].title);
-//   return (<Movie key={i}  {...info[1]} /*poster={info[1].poster_path} title={info[1].original_title} overview={info[1].overview} averageRating={info[1].vote_average} countRating={info[1].vote_count}*/ updateLikedMovies={updateLikedMovies} isLiked={isLiked} />)
-//  })
-
-  console.log(movie);
+    const isLiked = likedMovies.find((mov) => mov === info[1].title);
+    return (
+      <Movie
+        key={i}
+        className={styles.movieCard}
+        {...info[1]}
+        poster={`https://image.tmdb.org/t/p/w500${info[1].poster_path}`}
+        updateLikedMovies={
+          updateLikedMovies
+        }
+        isLiked={isLiked}
+      />
+    );
+  });
 
   let popoverContent = (
     <div className={styles.popoverContent}>{popoverMovies}</div>
   );
-  // const movie = infoMovie.map((info, i) => {
-  //   const isLiked = likedMovies.find(mov => mov === info.title);
-  //   return <Movie key={i} {...info} updateLikedMovies={updateLikedMovies} isLiked={isLiked} /*poster={info.poster} title={info.title} overview={info.overview} averageRating={info.averageRating} countRating={info.countRating}*/ />
-  // })
 
-    let el =likedMovies.length > 0 ? ` ♥ ${likedMovies.length} movie(s)`: "";
+  let el = likedMovies.length > 0 ? ` ♥ ${likedMovies.length} movie(s)` : "";
+
   return (
     <div className={styles.main}>
       <div className={styles.header}>
         <div className={styles.logocontainer}>
-        {/* <img src="logo.png" alt="Logo" /> */}
+          {/* <img src="logo.png" alt="Logo" /> */}
           <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
         </div>
-        <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
+        <Popover
+          title="Liked movies"
+          content={popoverContent}
+          className={styles.popover}
+          trigger="click"
+        >
           <Button>{el}</Button>
         </Popover>
       </div>
       <div className={styles.title}>LATEST RELEASES</div>
-      <div className={styles.moviesContainer}>
-        {movie}
-      </div>
+      <div className={styles.moviesContainer}>{movie}</div>
     </div>
   );
 }
--- components/Movie.jsx
+++ components/Movie.jsx
@@ -1,68 +1,93 @@
-import React, { useState } from 'react'
-import styles from "@/styles/Movie.module.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faVideo, faHeart } from '@fortawesome/free-solid-svg-icons';
-
+import React, { useState } from "react";
+import styles from "@/styles/Movie.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faVideo, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function Movie(props) {
-    const [personalNote, setNote] = useState(0);
-    const [watchCount, setCount] = useState(0);
-    const stars = [];
-    const {poster,  title, overview, vote_average, vote_count, updateLikedMovies, isLiked} = props 
-    for (let i = 0; i < 10; i++) {
-      let style = {}
-      if(i < vote_average - 1 ){
-        style = {color: "#f1c40f"}
-      }
-      stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style}/>);
+  const [personalNote, setNote] = useState(0);
+  const [watchCount, setCount] = useState(0);
+  const stars = [];
+  const {
+    poster,
+    title,
+    overview,
+    vote_average,
+    vote_count,
+    updateLikedMovies,
+    isLiked,
+  } = props;
+  for (let i = 0; i < 10; i++) {
+    let style = {};
+    if (i < vote_average - 1) {
+      style = { color: "#f1c40f" };
     }
-    
-    const handleClick = (a, b)=>{
-      setNote(b + 1)
-      console.log(b);
-    }
-    
-    const starmie = [];
-    for (let i = 0; i < 10; i++) {
-      let style = {}
-      if(i < personalNote){
-        style = {color: "#2196f3"}
-        }
-        starmie.push(<FontAwesomeIcon className={styles.icon} onClick={(event)=>handleClick(event, i)} key={i} icon={faStar} style={style}/>);
-      }
-      const viewClick = () => {
-        setCount(watchCount + 1)
-        
-    };
+    stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
+  }
 
-    
-    
-    
-    let heartStyle = { cursor: 'pointer' };
-    const likeClick = () => {
-      // shiftLike(!like)
-      updateLikedMovies(title)
-      console.log(title);
-      // console.log(updateLikedMovies(title));
-    };
-    if (isLiked) {
-      heartStyle = { color: 'red', cursor: 'pointer' };
+  const handleClick = (a, b) => {
+    setNote(b + 1);
+  };
+
+  const starmie = [];
+  for (let i = 0; i < 10; i++) {
+    let style = {};
+    if (i < personalNote) {
+      style = { color: "#2196f3" };
     }
-    
-    return (
-      <div className={styles.card}>
-        <img className={styles.image} src={poster} alt={title} />
-        <div className={styles.textContainer}>
-          <span className={styles.name}>{title}</span>
-          <p className={styles.descriptions}>{overview}</p>
-        </div>
-          <span className={styles.vote}>{stars} {vote_count}</span>
-          <span className={styles.vote}>{starmie} {personalNote}</span>
-          <span><FontAwesomeIcon className={styles.icon} onClick={() => viewClick()} icon={faVideo} style={{color: watchCount > 0 ? "#e74c3c" : "black"}}/>    {watchCount>0? watchCount:""}</span>
-          <span><FontAwesomeIcon className={styles.icon} onClick={()=> likeClick()} icon={faHeart} style={heartStyle}/></span>
-      </div>
+    starmie.push(
+      <FontAwesomeIcon
+        className={styles.icon}
+        onClick={(event) => handleClick(event, i)}
+        key={i}
+        icon={faStar}
+        style={style}
+      />
     );
   }
-  
-  export default Movie;
-  
+  const viewClick = () => {
+    setCount(watchCount + 1);
+  };
+
+  let heartStyle = { cursor: "pointer" };
+  const likeClick = () => {
+    updateLikedMovies(title);
+  };
+  if (isLiked) {
+    heartStyle = { color: "red", cursor: "pointer" };
+  }
+
+  return (
+    <div className={styles.card}>
+      <img className={styles.image} src={poster} alt={title} />
+      <div className={styles.textContainer}>
+        <span className={styles.name}>{title}</span>
+        <p className={styles.descriptions}>{overview}</p>
+      </div>
+      <span className={styles.vote}>
+        {stars} {vote_count}
+      </span>
+      <span className={styles.vote}>
+        {starmie} {personalNote}
+      </span>
+      <span>
+        <FontAwesomeIcon
+          className={styles.icon}
+          onClick={() => viewClick()}
+          icon={faVideo}
+          style={{ color: watchCount > 0 ? "#e74c3c" : "black" }}
+        />{" "}
+        {watchCount > 0 ? watchCount : ""}
+      </span>
+      <span>
+        <FontAwesomeIcon
+          className={styles.icon}
+          onClick={() => likeClick()}
+          icon={faHeart}
+          style={heartStyle}
+        />
+      </span>
+    </div>
+  );
+}
+
+export default Movie;
